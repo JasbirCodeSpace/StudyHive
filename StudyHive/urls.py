7ce44b4a2c749cf98ed1f0cae0269badecd4d8dd
Harsh <harshrai456@gmail.com>
2021-11-12 11:38:21 +0530

    DSA App added

--- StudyHive/urls.py
+++ StudyHive/urls.py
@@ -19,6 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dsa/', include('dsa.urls')),
     path('', include('accounts.urls')),
     path('', include('home.urls')),
     path('resource/', include('resource.urls')),
