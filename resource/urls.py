3ca5c84621d2b1d64861c112a9898e5839713b9a
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2021-11-19 23:22:32 +0530

    fetch resources view

--- resource/urls.py
+++ resource/urls.py
@@ -3,7 +3,8 @@ from resource.views import request, upload, resources
 
 
 urlpatterns = [
-    path('', resources.show_resources, name = 'resources'),
+    path('', resources.resources_view, name = 'resources'),
+    path('fetch/', resources.fetch_resources, name = 'fetch-resources'),
     path('request/', request.request_resource, name='resource-request'),
     path('upload/', upload.upload_resource, name='resource-upload'),
 ]
--- resource/views/resources.py
+++ resource/views/resources.py
@@ -3,9 +3,22 @@ from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from resource.models import course, subject, resource
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required(login_url='/login/')
-def show_resources(request):
+def resources_view(request):
     resources = resource.Resource.objects.all()
-    print(resources)
     return render(request, "resource/show.html", {'resources': resources})
+
+@login_required
+@csrf_exempt
+def fetch_resources(request):
+    resources = list(resource.Resource.objects.all().values())
+    resources = json.dumps(resources, cls=DjangoJSONEncoder)
+    return HttpResponse(resources, content_type="application/json")
+
+
+
+
